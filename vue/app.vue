9004d2e0edff2ab925a15c657ba5e4aaf918584c
mokupsas <mockus.ri@gmail.com>
2023-03-02 15:49:13 +0200

    created project

---
+++ vue/app.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="example">
+        <p>
+            Get speedtest running!
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style></style>
