efce6c28d2540750310ef5139e707b9044eb1b42
mokupsas <mockus.ri@gmail.com>
2023-03-06 17:00:17 +0200

    updated UI

--- vue/app.vue
+++ vue/app.vue
@@ -1,14 +1,86 @@
 <template>
-    <div class="example">
-        <p>
-            Get speedtest running!
-        </p>
-    </div>
+  <div class="wrapper-st">
+
+      <a-row>
+        <a-col :span="12">
+          <b>Download speed</b>
+          <p>{{ uploadSpeedAvg }} MB/s</p>
+          <speedometer class="speedometer" :value="downloadSpeed" max="100"></speedometer>
+        </a-col>
+        <a-col :span="12">
+          <b>Upload speed</b>
+          <p>{{ downloadSpeedAvg }} MB/s</p>
+          <speedometer :value="uploadSpeed" max="100"></speedometer>
+        </a-col>
+      </a-row>
+
+      <a-divider/>
+
+        <a-button v-if="!running" @click="doTest" class="space" type="primary">Start test</a-button>
+        <a-button v-if="running" class="space" type="primary" disabled>Start test</a-button>
+
+        <br/>
+        <a-space class="space">
+          <a-select ref="select" v-model="action" style="width: 120px">
+            <a-select-option v-for="(action, key) in actions" :key="key">{{ action }}</a-select-option>
+          </a-select>
+        </a-space>
+        <br/>
+        <a-textarea placeholder="Console log" :rows="4" disabled/>
+  </div>
 </template>
 
 <script>
+import Speedometer from './Speedometer.vue'
+
+const Action = {
+  AUTO: 0,
+  DOWNLOAD: 1,
+  UPLOAD: 2,
+  GET_LOCATION: 3
+}
+
 export default {
+  components: { Speedometer },
+  data () {
+    return {
+      running: false,
+
+      uploadSpeed: 0,
+      uploadSpeedAvg: 0,
+      downloadSpeed: 0,
+      downloadSpeedAvg: 0,
+
+      action: Action.AUTO,
+      actions: [
+        'Auto',
+        'Download',
+        'Upload',
+        'Get location'
+      ]
+    }
+  },
+  methods: {
+    doTest () {
+      this.$rpc.call('libspeedtest', 'test', {}).then(r => {
+        console.log(r)
+      })
+    }
+  }
 }
 </script>
 
-<style></style>
+<style>
+.wrapper-st {
+  width: 500px;
+  text-align: center;
+  justifyContent: 'center';
+}
+.speedometer {
+  margin-right: 15px;
+}
+
+.space {
+  margin-bottom: 10px;
+}
+</style>
---
+++ vue/components/Speedometer.vue
@@ -0,0 +1,35 @@
+<template>
+  <a-progress
+  type="circle"
+  :stroke-color="{
+    '0%': '#108ee9',
+    '100%': '#87d068',
+  }"
+  :format="percent => `${this.value} ${label}`"
+  status="normal"
+  :percent="countPercentage()"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Speedometer',
+  props: {
+    label: { default: 'MB/s' },
+    value: { required: true },
+    max: { default: 100 }
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    countPercentage () {
+      return Math.floor(this.value / this.max * 100)
+    }
+  }
+}
+</script>
+
+<style></style>
